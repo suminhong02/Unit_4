a50797788cf0459f798b594b391a20a93edd540c
suminhong02 <sumin.hong@ssfs.org>
2019-11-08 10:52:04 -0500

    Initial commit

---
+++ src/WhileLoops.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class WhileLoops {
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static String myMethod90{
+        scan.nextInt();
+    }
+    public static void main(String[] args){
+
+
+
+    }
+}
