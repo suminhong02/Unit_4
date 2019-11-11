a7bb0875a6f9694b947c4256aa11fa6f69004dd0
suminhong02 <sumin.hong@ssfs.org>
2019-11-11 12:05:36 -0500

    Working on WhileLoops

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -4,11 +4,19 @@ public class WhileLoops {
 
     static Scanner scan = new Scanner(System.in);
 
-    public static String myMethod90{
-        scan.nextInt();
+    public static String fromHereToThere(int a, int b){
+        String va = "";
+        while(a <= b){
+            if (a>b){
+                return "Invalid input";
+            }
+            va += a+" ";
+           a += 1;
+        }
+        return va;
     }
     public static void main(String[] args){
-
+        System.out.println(fromHereToThere(7,10));
 
 
     }
