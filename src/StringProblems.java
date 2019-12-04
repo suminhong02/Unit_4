6cc14e014c81ffaaf2840e1e88ff7cd318bf359f
suminhong02 <sumin.hong@ssfs.org>
2019-12-04 10:10:23 -0500

    Working on StringProblems

--- src/StringProblems.java
+++ src/StringProblems.java
@@ -20,16 +20,44 @@ public class StringProblems {
         int vowels = 0;
         s = s.toLowerCase();
         for(int i = 0; i < s.length(); i++){
-            if(s.substring(i, i+1) == "a" || s.substring(i, i+1) == "e" || s.substring(i, i+1) == "i"|| s.substring(i, i+1) == "o"|| s.substring(i, i+1) == "u"){
+            if(s.substring(i, i+1).equals("a") || s.substring(i, i+1).equals("e") || s.substring(i, i+1).equals("i")|| s.substring(i, i+1).equals("o")|| s.substring(i, i+1).equals("u")){
                 vowels += 1;
             }
+
         }
         return vowels;
+    }
+
+    public static String differentStrings(String s1, String s2){
+        String S = "";
+        String t1 = s1;
+        String t2 = s2;
+        int i = 0;
+        if (s1.equals(s2)){
+            return s1+" and "+s2+ " are the same.";
+        }
+        if (s1.length()>s2.length()){
+            t2+=" ";
+        }
+        else{
+            t1 +=" ";
+        }
+        while(t1.substring(i, i+1).equals (t2.substring(i, i+1))){
+            i++;
+        }
+        return s1+" and "+s2+" are not the same. They differ at number "+(i+1)+".";
+    }
+
+    public static boolean isPalindrome(String S){
+        String s = "";
+        int i = 0;
+        if ()
     }
 
     public static void main(String []args){
         //System.out.println(printTheLetters("Pizza"));
         //System.out.println(everyOtherLetter("Computer Science"));
-        System.out.println(countTheVowels("COMPUTER science"));
+        //System.out.println(countTheVowels("COMPUTER science"));
+        //System.out.println(differentStrings("tiger", "tiger"));
     }
 }
