053e20f5509a5e53707b87831835af3f1c6f90fa
suminhong02 <sumin.hong@ssfs.org>
2019-12-12 08:55:49 -0500

    Working on fizzbuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,24 @@
+
+public class FizzBuzz {
+    public static String fizzBuzz(int num){
+        String outcome = "";
+        for(int i = 1; i<=num; i++){
+
+            if(i%3 == 0 && i%5 == 0 && i>=15){
+                outcome += " FizzBuzz";
+            }
+            else if(i>=5 && i%5 == 0){
+                outcome += "Buzz";
+            }
+            else if(i>=3 && i%3 == 0){
+                outcome += "fizz";
+            }
+            else outcome += i;
+        }
+        return outcome;
+    }
+    public static void main(String[]args){
+        System.out.println(fizzBuzz(100));
+    }
+}
+
